e5599b01af49e1b0ad77b4fd80053794119d6194
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-03-12 21:42:18 -0600

    added new schema and seeds files

--- db/schema.sql
+++ db/schema.sql
@@ -1,31 +1,29 @@
 DROP DATABASE IF EXISTS tracker_db;
--- DROP TABLE IF EXISTS employee;
-DROP TABLE IF EXISTS role;
-DROP TABLE IF EXISTS department;
 
 CREATE DATABASE tracker_db;
 USE tracker_db;
 
 
-CREATE TABLE department (
-    id INTEGER NOT NULL auto_increment PRIMARY KEY,
-    department VARCHAR(30)
+CREATE TABLE departments (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
-    title VARCHAR(30),
-    salary DECIMAL PRIMARY KEY,
-    role_id INTEGER,
-    department VARCHAR(30)
+CREATE TABLE roles (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER(11) NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
-CREATE TABLE employee (
-  id INTEGER NOT NULL auto_increment PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  title VARCHAR(30),
-  department VARCHAR(30), 
-  salary DECIMAL,
-  manager VARCHAR(30)
+CREATE TABLE employees (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER(11) NULL,
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,32 +1,27 @@
 USE tracker_db;
 
-INSERT into department (department) VALUES ('Sales');
-INSERT into department (department) VALUES ('IT');
-INSERT into department (department) VALUES ('Legal');
-INSERT into department (department) VALUES ("HR");
+INSERT INTO departments(name) VALUES
+('Sales'),
+('IT'),
+('Legal'),
+('HR');
 
-INSERT into role (title, salary, role_id, department) VALUES ("Sales Lead", 60000, 1, "Sales");
-INSERT into role  (title, salary, role_id, department) VALUES ("Salesperson", 50000, 1, "Sales");
-INSERT into role (title, salary, role_id, department) VALUES ("Lead Engineer", 120000, 2, "IT");
-INSERT into role (title, salary, role_id, department) VALUES ("Software Engineer", 90000, 2, "IT");
-INSERT into role (title, salary, role_id, department) VALUES ("Accountant", 95000, 1, "Sales");
-INSERT into role (title, salary, role_id, department) VALUES ("Legal Team Lead", 100000, 3, "Legal");
-INSERT into role (title, salary, role_id, department) VALUES ("Lawyer", 190000, 3, "Legal");
-INSERT into role (title, salary, role_id, department) VALUES ("Cyber Security", 160000, 2, "IT");
-INSERT into role (title, salary, role_id, department) VALUES ("Full Stack Developer", 130000, 2, "IT");
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Sales Lead', 60000, 1),
+        ('Salesperson', 50000, 1),
+        ('Lead Engineer', 120000, 2),
+        ('Accountant', 95000, 1),
+        ('Assistant', 46000, 4),
+        ('Lawyer', 100000, 3);
 
-
-INSERT into employee (first_name, last_name, manager, department, title, salary) VALUES ("Ronald", "Firbank", "Bo", "Sales", "Sales Lead", 90000);
-INSERT into employee (first_name, last_name, manager, department, title, salary) VALUES ("Aaliyah", "Symlar", "Silk", "IT", "Lead Engineer", 120000);
-INSERT into employee (first_name, last_name, manager, department, title, salary) VALUES ("Piers", "Gaveston", "Bob", "Legal", "Lawyer", 190000);
-
-
-INSERT into employee (first_name, last_name, manager, department, title, salary) VALUES ("Brandon", "Speers", "Ryan", "Sales", "Accountant", 95000);
-INSERT into employee (first_name, last_name, manager, department, title, salary) VALUES ("King ", "T'Challa", "Charles", "Legal", "Legal Team Lead", 100000);
-INSERT into employee (first_name, last_name, manager, department, title, salary) VALUES ("Thor", null, "Mike", "IT", "Software Engineer", 100000);
-INSERT into employee (first_name, last_name, manager, department, title, salary) VALUES ("Virginia", "Woolf", "Kim", "IT", "Cyber Security", 160000);
-
-
+INSERT INTO employees(first_name, last_name, role_id, manager_id)
+VALUES('Ronald', 'Firbank', 1, null),
+('Aaliyah', 'Symlar', 2, 1),
+('Anasia', 'Doe', 2, 1),
+('Piers', 'Gaveston', 3, null),
+('Atlas', 'Omni', 4, 4),
+('Samra', 'Till', 4, 4),
+('Scarlett', 'Johansson', 5, null);
 
 
 
